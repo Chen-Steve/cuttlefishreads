614d0aadd47dd01fef3f69d67038c7b440422f41
Chen-Steve <zhangc2@illinois.edu>
2026-09-02 11:27:16 -0400

    update

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -11,7 +11,7 @@ export function getStripe(): Stripe {
   }
   if (!stripeClient) {
     stripeClient = new Stripe(key, {
-      apiVersion: "2026-07-29.dahlia",
+      apiVersion: "2026-08-26.dahlia",
       typescript: true,
     });
   }
